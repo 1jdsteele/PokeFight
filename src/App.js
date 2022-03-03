8b79afbd5d40ce038a989a6418c4220430dc5673
jdsteele <jdsteelepersonal@gmail.com>
2022-03-03 11:35:27 -0800

    form submits and console logs data specified by user

--- src/App.js
+++ src/App.js
@@ -2,12 +2,57 @@
 import './App.css';
 import Team1 from './components/team1/team1';
 import SubmissionForm from './components/submissionform/submissionform';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [userInput, setUserInput] = useState(1);  //this is the state that goes from user input into the API request
+    
+  const[nextPoke, setNextPoke] = useState(); //this is the state that holds data from api request and sends it to Team1
+
+  function requestData(userInput) {
+
+      const url = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
+
+      //THE ERROR
+      //Uncaught (in promise) SyntaxError: Unexpected token N in JSON at position 0
+      // IS FROM THIS FETCH
+      fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setNextPoke(data); // this is all the data for the poke
+            console.log(data);
+            setUserInput('');  
+          })
+          .catch(console.log.error);
+  }  
+
+  useEffect(() => {
+    requestData(userInput);
+  }, []);
+
+  function handleChange(event) {
+    event.preventDefault(); // THIS LINE MIGHT NOT BE NECESSARY KEEP FOR NOW BUT AT END CHECK
+    setUserInput(event.target.value);
+
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //THIS UPCOMING LINE MIGHT BE NECESSARY BUT I DON'T THINK SO
+    //requestData();
+    requestData(userInput);
+
+  }
+
   return (
     <div>
       <h2>app has loaded properly</h2>
-      <SubmissionForm />
+      <SubmissionForm 
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        userInput={userInput}
+      />
       <Team1 />
     </div>
   );
--- src/components/submissionform/submissionform.js
+++ src/components/submissionform/submissionform.js
@@ -1,23 +1,56 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-export default function SubmissionForm() {
 
-    const [userinput, setUserInput] = useState();
 
-    function handleClick(event) {
-        event.preventDefault();
-        setUserInput("example3");
+export default function SubmissionForm({handleSubmit, handleChange, searchString }) {
 
-    }
+    //LINES 9-24 ALREADY IN APP.JS - TO MAKE WORK UNCOMMENT OUT THAT AND LINE 28
+
+    // const [userInput, setUserInput] = useState();  //this is the state that goes from user input into the API request
+    
+    // const[nextPoke, setNextPoke] = useState(); //this is the state that holds data from api request and sends it to Team1
+
+    // function requestData(userInput) {
+
+    //     const url = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
+
+    //     fetch(url)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             let nextPoke = data.name // YO JAKE CHECK AND FIX THIS LINE
+    //         })
+    //         .catch(console.log.error);
+    // }
+
+//     function HandleClick(event) {
+//         event.preventDefault();
+//         // setUserInput("example3");
+//         //this is where the logic to move the state (since it was already changed by handle change) to the url and fetch shoould accur
+//         //and then fetch in turn will...display the data? check birds I guess
+        
+
+
+    
+        
+
+// ;
+
+//     }
+//      function handleChange(event) {
+//          event.preventDefault();
+//         //  setUserInput(event.target.value);
+//      }
 
     return (
         <>
-        <h2>submission form has loaded</h2>
-        <form action="">
+        
+        <form onSubmit={handleSubmit}>
             <label htmlFor="userInput">Type in name or ID number</label>
-            <input type="text" id="userInput" />
-            <button onClick={handleClick}>Submit</button>
+            <input type="text" id="userInput" onChange={handleChange}/>
+            <button>Submit</button>
         </form>
+        
         </>
     )
 }
